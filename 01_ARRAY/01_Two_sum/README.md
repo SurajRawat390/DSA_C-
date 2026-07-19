827b3b7ec9b44e0756802c36802002b939c90c9f
Suraj Rawat <surajrawat2212007@gmail.com>
2026-07-19 15:47:04 +0530

    Two sum problem solve with Hash map.

---
+++ 01_ARRAY/01_Two_sum/README.md
@@ -0,0 +1,8 @@
+01. Two sum problem.
+
+Solution : We have given an array of integers nums and a target value we have to return the index of two numbers so that the sum of the two number is equal to the target value.
+
+Approach : We are using the Hash map approach.
+
+Time complexity = O(n)
+Space complexity = O(n)
---
+++ 01_ARRAY/01_Two_sum/code.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+vector<int> two_sum(vector<int>&nums,int target){
+    unordered_map<int,int>m;
+
+    for(int i = 0;i<nums.size();i++){
+        int x = nums[i];
+        int y = target - x;
+
+        if(m.find(y) != m.end() ){
+            return {m[y],i};
+        }
+        m[x] = i;
+    }
+    return {};
+}
+int main()
+{
+    vector<int>nums = {2,7,11,15};
+    int target = 9;
+    vector<int>ans = two_sum(nums,target);
+    for(int x : ans){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
